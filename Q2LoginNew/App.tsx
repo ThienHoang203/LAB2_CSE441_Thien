e8215e17f3e4f9831d801e929082678228586ff0
ThienHoang203 <thien.hoangcong.203@gmail.com>
2024-10-18 11:02:48 +0700

    q3 commit

---
+++ Q2LoginNew/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import LoginScreen from './Login/Login';
+
+
+export default App = () => {
+  return (
+    <LoginScreen />
+  );
+};
